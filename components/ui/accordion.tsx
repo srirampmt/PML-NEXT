dbecfe18faa74c102782e521a1d81607b5c75a65
Sriram <srirampmt@gmail.com>
2025-12-10 19:47:02 +0530

    10122025

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
-import { ChevronDown } from 'lucide-react';
+import { Plus, Minus } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
@@ -28,13 +28,16 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between py-5 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+        'flex flex-1 items-center justify-between py-5 font-medium transition-all hover:underline group',
         className
       )}
       {...props}
     >
       {children}
-      <ChevronDown className="h-5 w-5 font-extrabold shrink-0 transition-transform duration-200" />
+      <div className="shrink-0 rounded-full border-2 border-current transition-all duration-500 ease-in-out relative flex items-center justify-center w-8 h-8">
+        <Plus className="h-4 w-4 absolute transition-all duration-500 ease-in-out group-data-[state=open]:rotate-180 group-data-[state=open]:opacity-0 group-data-[state=open]:scale-50" strokeWidth={2.5} />
+        <Minus className="h-4 w-4 absolute transition-all duration-500 ease-in-out group-data-[state=closed]:rotate-180 group-data-[state=closed]:opacity-0 group-data-[state=closed]:scale-50" strokeWidth={2.5} />
+      </div>
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
